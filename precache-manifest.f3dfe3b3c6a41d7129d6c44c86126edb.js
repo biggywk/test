a8bd327cf4a14e4bb4e4bd21713a698a96b97f64
big.baby-@hotmail.com <30289807+biggywk@users.noreply.github.com>
2019-05-20 17:06:12 +0700

    Updates

---
+++ precache-manifest.f3dfe3b3c6a41d7129d6c44c86126edb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03d8714bcf7ae3edb099abca05dc7dd",
+    "url": "/biggywk/test/index.html"
+  },
+  {
+    "revision": "d7dd6854dfa4d513713f",
+    "url": "/biggywk/test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/biggywk/test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d7dd6854dfa4d513713f",
+    "url": "/biggywk/test/static/js/main.7e18d307.chunk.js"
+  },
+  {
+    "revision": "35c63a77f78ce237f704",
+    "url": "/biggywk/test/static/js/runtime~main.752c85e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/biggywk/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
